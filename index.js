53976f8b65d0529001d76823db3ae4cef663a672
gvluu <121136183+gvluu@users.noreply.github.com>
2023-02-23 14:46:29 -0800

    Add return statement to 'getComputerChoice'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const options = ["rock", "paper", "scissors"];
 
-function getComputerChoice () {
+function getComputerChoice() {
     const computerChoice = options[Math.floor(Math.random() * options.length)];
     // to generate random integer between 0 & length of options
     // could also have done * 3 since that's how many elements in the array
@@ -12,62 +12,45 @@ function getComputerChoice () {
       } else {
         return "scissors";
       }  */
-    console.log(computerChoice);
+    console.log(computerChoice)
+    return computerChoice; 
+    // NOTE: Return = OUTCOME. Console.log = OUTPUT (for debugging)
+    // NEED THIS RETURN STATEMENT! It was missing the whole time. That's why I kept only getting ELSE response. 
+    // Prior, it was only logging a random response to the console but it did not return a value. Now it can be used in 'computerSelection' under 'playRound'.
 }
 
 function playRound(playerSelection, computerSelection) {
     // case-insensitive so ANY variation of rock, paper, or scissors will be recognized.
-    playerSelection = playerSelection.toLowerCase();
-    let tie = "It's a tie! You selected " + playerSelection + " and the computer chose " + computerSelection + "!";
-    let rockLosePaper = "You Lose! You selected " + playerSelection + " and the computer chose " + computerSelection + "!";
-    let rockBeatScissors = "You Win! You selected " + playerSelection + " and the computer chose " + computerSelection + "!";
-    let paperLoseScissors = "You Lose! You selected " + playerSelection + " and the computer chose " + computerSelection + "!";
-    let paperBeatRock = "You Win! You selected " + playerSelection + " and the computer chose " + computerSelection + "!";
-    let scissorLoseRock = "You Lose! You selected " + playerSelection + " and the computer chose " + computerSelection + "!";
-    let scissorBeatPaper = "You Win! You selected " + playerSelection + " and the computer chose " + computerSelection + "!";
+    playerSelection = playerSelection.toLowerCase()
+    let tie = "It's a tie! You selected " + playerSelection + " and the computer chose " + computerSelection + "!"
+    let rockLosePaper = "You Lose! You selected " + playerSelection + " and the computer chose " + computerSelection + "!"
+    let rockBeatScissors = "You Win! You selected " + playerSelection + " and the computer chose " + computerSelection + "!"
+    let paperLoseScissors = "You Lose! You selected " + playerSelection + " and the computer chose " + computerSelection + "!"
+    let paperBeatRock = "You Win! You selected " + playerSelection + " and the computer chose " + computerSelection + "!"
+    let scissorLoseRock = "You Lose! You selected " + playerSelection + " and the computer chose " + computerSelection + "!"
+    let scissorBeatPaper = "You Win! You selected " + playerSelection + " and the computer chose " + computerSelection + "!"
 
-    if (playerSelection === 'rock') {
-      if (computerSelection === 'paper') {
+    if (playerSelection === computerSelection) {
+        return tie;
+    } else if ((playerSelection === "rock") && (computerSelection === "paper")) {
         return rockLosePaper;
-      } else if (computerSelection === 'scissors') {
+    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
         return rockBeatScissors;
-      } else {
-        return tie;
-      }
-    } else if (playerSelection === 'paper') {
-      if (computerSelection === 'scissors') {
+    } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
         return paperLoseScissors;
-      } else if (computerSelection === 'rock') {
+    } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
         return paperBeatRock;
-      } else {
-        return tie;
-      }
-    } else if (playerSelection === 'scissors') {
-      if (computerSelection === 'rock') {
+    } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
         return scissorLoseRock;
-      } else if (computerSelection === 'paper') {
+    } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
         return scissorBeatPaper;
-      } else {
-        return tie;
-      }
     } else {
-      // If invalid playerSelection
-      return 'Invalid selection! Please choose Rock, Paper, or Scissors.';
+    // If invalid playerSelection
+    return("Invalid selection! Please choose rock, paper, or scissors.")
     }
-  }
+}                                                        
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
+  console.log(playerSelection);
   console.log(playRound(playerSelection, computerSelection));
 
-
-  // to be able to play 5 rounds. i could also start at 1
-  function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(play(playerSelection, computerSelection));
-    }
-  }
-    
